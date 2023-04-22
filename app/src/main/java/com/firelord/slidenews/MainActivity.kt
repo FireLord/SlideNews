26d4be483501795460aa50bfc6c8e34d7c68b311
FireLord <firelord.xda@gmail.com>
2023-04-22 09:23:10 +0530

    app: add navhost and setup basic navigation
    -updated gradle to 8.0
    -switched to latest java 17
    
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- app/src/main/java/com/firelord/slidenews/MainActivity.kt
+++ app/src/main/java/com/firelord/slidenews/MainActivity.kt
@@ -2,10 +2,21 @@ package com.firelord.slidenews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.firelord.slidenews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(
+            navController
+        )
     }
 }
