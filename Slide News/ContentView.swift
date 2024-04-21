8294364f8ac2ac97aafb317aeed22ed80c04e8f6
FireLord <firelord.xda@gmail.com>
2024-04-21 17:15:45 +0530

    first commit

---
+++ Slide News/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slide News
+//
+//  Created by Aman Kumar on 21/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slide News/Slide_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slide_NewsApp.swift
+//  Slide News
+//
+//  Created by Aman Kumar on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Slide_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
