f94377352e35ee34dac373494b7ab125363680e3
FireLord <firelord.xda@gmail.com>
2024-04-28 02:59:41 +0530

    loginView: Put the view in navigationStack

--- Slide News/presentation/screen/onboarding/LoginView.swift
+++ Slide News/presentation/screen/onboarding/LoginView.swift
@@ -17,96 +17,99 @@ struct LoginView: View {
     }
     
     var body: some View {
-        ZStack {
-            Color.primaryTwo
-            
-            VStack(alignment: .leading) {
-                Spacer()
+        NavigationStack {
+            ZStack {
+                Color.primaryTwo
                 
-                Text("Hey,\nWelcome To\nSlide News")
-                    .font(.outfitFont(.medium, fontSize: .heading))
-                    .foregroundStyle(.black)
-                    .padding()
-                    .padding(.top, 40)
-                
-                ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
-                    RoundedRectangle(cornerRadius: 20)
-                        .fill(.grayPrimary)
-                        .shadow(radius: 10, y: -10)
+                VStack(alignment: .leading) {
+                    Spacer()
                     
-                    VStack {
-                        Button {
-                            print("hi")
-                        } label: {
-                            Text("Skip")
-                                .font(.outfitFont(.regular, fontSize: .subHeadline))
-                                .underline()
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                .foregroundStyle(.white)
-                                .padding(.horizontal, 30)
-                                .padding(.top, 30)
-                        }
-                        
-                        InputView(
-                            text: $email,
-                            title: "Email Address",
-                            placeholder: "xyz@gmail.com"
-                        )
-                        .focused($focusedTextField, equals: .email)
-                        .onSubmit {
-                            focusedTextField = .password
-                        }
-                        .submitLabel(.continue)
+                    Text("Hey,\nWelcome To\nSlide News")
+                        .font(.outfitFont(.medium, fontSize: .heading))
+                        .foregroundStyle(.black)
                         .padding()
+                        .padding(.top, 40)
+                    
+                    ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.grayPrimary)
+                            .shadow(radius: 10, y: -10)
                         
-                        InputView(
-                            text: $password,
-                            title: "Password",
-                            placeholder: "*****",
-                            isSecureField: true
-                        )
-                        .focused($focusedTextField, equals: .password)
-                        .onSubmit {
-                            focusedTextField = nil
-                        }
-                        .submitLabel(.done)
-                        .padding()
-                        
-                        Button {
-                            
-                        } label: {
-                            LoginButton(name: "LOGIN IN")
-                        }
-                        .padding(.horizontal)
-                        
-                        Button {
-                            
-                        } label: {
-                            LoginButton(name: "GOOGLE SIGN IN")
-                        }
-                        .padding(.horizontal)
-                        
-                        
-                        Button {
-                            print("hi")
-                        } label: {
-                            HStack(spacing: 5) {
-                                Text("Don't have an account?")
-                                Text("Sign up")
-                                    .fontWeight(.bold)
+                        VStack {
+                            NavigationLink {
+                                MainTabView()
+                                    .navigationBarBackButtonHidden()
+                            } label: {
+                                Text("Skip")
+                                    .font(.outfitFont(.regular, fontSize: .subHeadline))
+                                    .underline()
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                    .foregroundStyle(.white)
+                                    .padding(.horizontal, 30)
+                                    .padding(.top, 30)
                             }
-                            .font(.outfitFont(.regular, fontSize: .subHeadline))
-                            .foregroundStyle(.white)
+                            
+                            InputView(
+                                text: $email,
+                                title: "Email Address",
+                                placeholder: "xyz@gmail.com"
+                            )
+                            .focused($focusedTextField, equals: .email)
+                            .onSubmit {
+                                focusedTextField = .password
+                            }
+                            .submitLabel(.continue)
+                            .padding()
+                            
+                            InputView(
+                                text: $password,
+                                title: "Password",
+                                placeholder: "*****",
+                                isSecureField: true
+                            )
+                            .focused($focusedTextField, equals: .password)
+                            .onSubmit {
+                                focusedTextField = nil
+                            }
+                            .submitLabel(.done)
+                            .padding()
+                            
+                            Button {
+                                
+                            } label: {
+                                LoginButton(name: "LOGIN IN")
+                            }
+                            .padding(.horizontal)
+                            
+                            Button {
+                                
+                            } label: {
+                                LoginButton(name: "GOOGLE SIGN IN")
+                            }
+                            .padding(.horizontal)
+                            
+                            
+                            Button {
+                                print("hi")
+                            } label: {
+                                HStack(spacing: 5) {
+                                    Text("Don't have an account?")
+                                    Text("Sign up")
+                                        .fontWeight(.bold)
+                                }
+                                .font(.outfitFont(.regular, fontSize: .subHeadline))
+                                .foregroundStyle(.white)
+                            }
+                            .padding()
+                            
+                            Spacer()
                         }
-                        .padding()
-                        
-                        Spacer()
                     }
+                    
                 }
-                
             }
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
     }
 }
 
