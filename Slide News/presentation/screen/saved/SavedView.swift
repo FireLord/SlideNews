6c19d30b9de09783acf0f1b752bd344c82357e85
FireLord <firelord.xda@gmail.com>
2024-04-28 15:48:19 +0530

    saved/search: Open the detail Screen

--- Slide News/presentation/screen/saved/SavedView.swift
+++ Slide News/presentation/screen/saved/SavedView.swift
@@ -14,82 +14,86 @@ struct SavedView: View {
     @State var searchQuery: String = ""
     
     var body: some View {
-        VStack(alignment: .leading) {
-            // Title
-            Text("Saved News")
-                .font(.outfitFont(.medium, fontSize: .largeTitle))
-                .foregroundStyle(.white)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-            
-            // Search
-            HStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(.grayPrimary)
-                    .frame(height: 50)
-                    .overlay {
-                        TextField("Search saved news", text: $searchQuery)
-                            .font(.outfitFont(.regular, fontSize: .title3))
-                            .submitLabel(.search)
-                            .padding(.leading)
-                    }
+        NavigationStack {
+            VStack(alignment: .leading) {
+                // Title
+                Text("Saved News")
+                    .font(.outfitFont(.medium, fontSize: .largeTitle))
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
                 
-                ZStack {
-                    Circle()
+                // Search
+                HStack {
+                    RoundedRectangle(cornerRadius: 25)
                         .fill(.grayPrimary)
-                        .frame(width: 50)
+                        .frame(height: 50)
+                        .overlay {
+                            TextField("Search saved news", text: $searchQuery)
+                                .font(.outfitFont(.regular, fontSize: .title3))
+                                .submitLabel(.search)
+                                .padding(.leading)
+                        }
                     
-                    Button {
-                        print("hi")
-                    } label: {
+                    ZStack {
                         Circle()
-                            .fill(.graySecondary)
-                            .frame(width: 40)
-                            .shadow(radius: 10)
-                            .overlay {
-                                Image(systemName: "magnifyingglass")
-                                    .scaleEffect(1.2)
-                                    .foregroundStyle(.white)
-                            }
+                            .fill(.grayPrimary)
+                            .frame(width: 50)
+                        
+                        Button {
+                            print("hi")
+                        } label: {
+                            Circle()
+                                .fill(.graySecondary)
+                                .frame(width: 40)
+                                .shadow(radius: 10)
+                                .overlay {
+                                    Image(systemName: "magnifyingglass")
+                                        .scaleEffect(1.2)
+                                        .foregroundStyle(.white)
+                                }
+                        }
                     }
                 }
-            }
-            .padding(.horizontal)
-            
-            // Saved news card
-            ZStack {
-                ForEach(0 ..< appViewModel.articleDbList.count, id: \.self) { index in
-                    let colorIndex = index % NewsCardColor.cardColorList.count
-                    let color = NewsCardColor.cardColorList[colorIndex]
-                    
-                    NewsCard(
-                        article: appViewModel.articleDbList[index],
-                        newsCardColor: color,
-                        x: CGFloat(index * -15),
-                        y: CGFloat(index * 200),
-                        degree: CGFloat(index * -5),
-                        onSwipeOut: {
-                            print("hi")
-                        }, 
-                        onSave: { article in
-                            Task {
-                                try await appViewModel.deleteSavedNews(article: article)
-                            }
+                .padding(.horizontal)
+                
+                // Saved news card
+                ZStack {
+                    ForEach(0 ..< appViewModel.articleDbList.count, id: \.self) { index in
+                        let colorIndex = index % NewsCardColor.cardColorList.count
+                        let color = NewsCardColor.cardColorList[colorIndex]
+                        NavigationLink {
+                            DetailView(article: appViewModel.articleDbList[index], newsCardColor: color)
+                        } label: {
+                            NewsCard(
+                                article: appViewModel.articleDbList[index],
+                                newsCardColor: color,
+                                x: CGFloat(index * -15),
+                                y: CGFloat(index * 200),
+                                degree: CGFloat(index * -5),
+                                onSwipeOut: {
+                                    print("hi")
+                                },
+                                onSave: { article in
+                                    Task {
+                                        try await appViewModel.deleteSavedNews(article: article)
+                                    }
+                                }
+                            )
                         }
-                    )
-                    
+                    }
                 }
-            }
-            .onAppear {
-                Task {
-                    try await appViewModel.getAllSavedNews()
+                .onAppear {
+                    Task {
+                        try await appViewModel.getAllSavedNews()
+                    }
                 }
+                Spacer()
             }
-            Spacer()
         }
     }
 }
 
 #Preview {
-    SavedView()
+    SavedView().environmentObject(AppViewModel())
 }
--- Slide News/presentation/screen/search/SearchView.swift
+++ Slide News/presentation/screen/search/SearchView.swift
@@ -12,77 +12,83 @@ struct SearchView: View {
     @State var searchQuery: String = ""
     
     var body: some View {
-        VStack(alignment: .leading) {
-            // Title
-            Text("Search News")
-                .font(.outfitFont(.medium, fontSize: .largeTitle))
-                .foregroundStyle(.white)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                .padding(.top, 50)
-            
-            // Search
-            HStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(.grayPrimary)
-                    .frame(height: 50)
-                    .overlay {
-                        TextField("Search news", text: $searchQuery)
-                            .font(.outfitFont(.regular, fontSize: .title3))
-                            .submitLabel(.search)
-                            .onSubmit {
-                                Task {
-                                    await appViewModel.getSearchNews(searchQuery: searchQuery)
-                                }
-                            }
-                            .padding(.leading)
-                    }
+        NavigationStack {
+            VStack(alignment: .leading) {
+                // Title
+                Text("Search News")
+                    .font(.outfitFont(.medium, fontSize: .largeTitle))
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    .padding(.top, 50)
                 
-                ZStack {
-                    Circle()
+                // Search
+                HStack {
+                    RoundedRectangle(cornerRadius: 25)
                         .fill(.grayPrimary)
-                        .frame(width: 50)
-                    
-                    Button {
-                        Task {
-                            await appViewModel.getSearchNews(searchQuery: searchQuery)
+                        .frame(height: 50)
+                        .overlay {
+                            TextField("Search news", text: $searchQuery)
+                                .font(.outfitFont(.regular, fontSize: .title3))
+                                .submitLabel(.search)
+                                .onSubmit {
+                                    Task {
+                                        await appViewModel.getSearchNews(searchQuery: searchQuery)
+                                    }
+                                }
+                                .padding(.leading)
                         }
-                    } label: {
+                    
+                    ZStack {
                         Circle()
-                            .fill(.graySecondary)
-                            .frame(width: 40)
-                            .shadow(radius: 10)
-                            .overlay {
-                                Image(systemName: "magnifyingglass")
-                                    .scaleEffect(1.2)
-                                    .foregroundStyle(.white)
-                            }
-                    }
-                }
-            }
-            .padding(.horizontal)
-            
-            // Search Card
-            ScrollView {
-                LazyVStack {
-                    ForEach(0 ..< appViewModel.articleFetchList.count, id: \.self) { index in
-                        let colorIndex = index % NewsCardColor.cardColorList.count
-                        let color = NewsCardColor.cardColorList[colorIndex]
+                            .fill(.grayPrimary)
+                            .frame(width: 50)
                         
-                        SearchCardItem(article: appViewModel.articleFetchList[index], newsCardColor: color)
+                        Button {
+                            Task {
+                                await appViewModel.getSearchNews(searchQuery: searchQuery)
+                            }
+                        } label: {
+                            Circle()
+                                .fill(.graySecondary)
+                                .frame(width: 40)
+                                .shadow(radius: 10)
+                                .overlay {
+                                    Image(systemName: "magnifyingglass")
+                                        .scaleEffect(1.2)
+                                        .foregroundStyle(.white)
+                                }
+                        }
                     }
                 }
+                .padding(.horizontal)
+                
+                // Search Card
+                ScrollView {
+                    LazyVStack {
+                        ForEach(0 ..< appViewModel.articleFetchList.count, id: \.self) { index in
+                            let colorIndex = index % NewsCardColor.cardColorList.count
+                            let color = NewsCardColor.cardColorList[colorIndex]
+                            
+                            NavigationLink {
+                                DetailView(article: appViewModel.articleFetchList[index], newsCardColor: color)
+                            } label: {
+                                SearchCardItem(article: appViewModel.articleFetchList[index], newsCardColor: color)
+                            }
+                        }
+                    }
+                }
+                .alert(item: $appViewModel.alertItem) { alertItem in
+                    Alert(title: alertItem.title,
+                          message: alertItem.message,
+                          dismissButton: alertItem.dismissButton)
+                }
+                .padding(.top)
+                
+                Spacer()
             }
-            .alert(item: $appViewModel.alertItem) { alertItem in
-                Alert(title: alertItem.title,
-                      message: alertItem.message,
-                      dismissButton: alertItem.dismissButton)
-            }
-            .padding(.top)
-            
-            Spacer()
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
     }
 }
 
